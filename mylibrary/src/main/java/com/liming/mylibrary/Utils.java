192a244d7694bb85555a52b1e844e6d80a0d0581
Liming 傅梨明 <liming_fu@intretech.com>
2018-07-10 10:10:36 +0800

    1.增加注释

--- mylibrary/src/main/java/com/liming/mylibrary/Utils.java
+++ mylibrary/src/main/java/com/liming/mylibrary/Utils.java
@@ -5,7 +5,12 @@ package com.liming.mylibrary;
  */
 
 public class Utils {
-
+    /**
+     * 两个整型相加方法
+     * @param x  参数1
+     * @param y  参数2
+     * @return
+     */
     public static int add(int x, int y) {
         return x + y;
     }
