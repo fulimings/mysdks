e0669e6c53a2f7086ffd444ad5d406063e34193f
Liming 傅梨明 <liming_fu@intretech.com>
2018-07-10 10:22:16 +0800

    1.增加注释保留配置

--- mylibrary/src/main/java/com/liming/mylibrary/Utils.java
+++ mylibrary/src/main/java/com/liming/mylibrary/Utils.java
@@ -1,5 +1,7 @@
 package com.liming.mylibrary;
 
+import android.util.Log;
+
 /**
  * Created by YQ04150 on 2018/7/10 0010.
  */
@@ -7,11 +9,23 @@ package com.liming.mylibrary;
 public class Utils {
     /**
      * 两个整型相加方法
-     * @param x  参数1
-     * @param y  参数2
+     *
+     * @param x 参数1
+     * @param y 参数2
      * @return
      */
     public static int add(int x, int y) {
         return x + y;
     }
+
+    /**
+     * 日志打印
+     * @param TAG   标签
+     * @param msg   日志内容
+     */
+
+    public static void logs(String TAG, String msg) {
+        Log.d(TAG, msg);
+    }
+
 }
