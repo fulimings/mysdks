f998ca632ceab3eb638b1c3f51f2bc2d2acd4950
Liming 傅梨明 <liming_fu@intretech.com>
2018-07-10 09:59:51 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/liming/mylibrary/Utils.java
@@ -0,0 +1,12 @@
+package com.liming.mylibrary;
+
+/**
+ * Created by YQ04150 on 2018/7/10 0010.
+ */
+
+public class Utils {
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+}
